d23a4fc2da2fe6faa5dd07afebd0b77cf5f466fd
evijidmmkf <3117758508@qq.com>
2022-11-07 16:06:04 +0800

    改变了mainContents文件中的filesArea less块位置

--- src/components/Markdown/mainContents.vue
+++ src/components/Markdown/mainContents.vue
@@ -75,57 +75,58 @@ export default {
     const files = [
       {
         id: 1,
-        name: 'f1',
+        name: "f1",
         children: [
           {
             id: 11,
-            name: 'f11',
+            name: "f11",
             children: [
               {
                 id: 111,
-                name: 'f111',
-              }, {
+                name: "f111",
+              },
+              {
                 id: 112,
-                name: 'f112'
-              }
-            ]
+                name: "f112",
+              },
+            ],
           },
           {
             id: 12,
-            name: 'f12'
-          }
-        ]
+            name: "f12",
+          },
+        ],
       },
       {
         id: 2,
-        name: 'f2',
+        name: "f2",
         children: [
           {
             id: 21,
-            name: 'f21',
+            name: "f21",
             children: [
               {
                 id: 211,
-                name: 'f211',
+                name: "f211",
               },
-            ]
-          }
-        ]
+            ],
+          },
+        ],
       },
       {
         id: 3,
-        name: 'f3',
+        name: "f3",
         children: [
           {
             id: 31,
-            name: 'f31'
+            name: "f31",
           },
           {
             id: 32,
-            name: 'f32'
-          }
-        ]
-      }
+            name: "f32",
+          },
+        ],
+      },
     ];
     // markdown基本设置
     marked.setOptions({
@@ -256,7 +257,7 @@ export default {
       editor.on("scroll", useEdiScroll);
       mdStore().md = editor;
     });
-    return { aftcontent, l, fillNum, usePreScroll, mdStore,files };
+    return { aftcontent, l, fillNum, usePreScroll, mdStore, files };
   },
 };
 </script>
@@ -266,6 +267,15 @@ export default {
   position: relative;
   width: 100%;
   height: calc(100vh - 160px);
+  .filesArea {
+      position: absolute;
+      left: -20%;
+      top: 0;
+      width: 20%;
+      height: 100%;
+      background-color: #fff;
+      transition: all 0.5s;
+    }
   .mainEdiPlace {
     position: absolute;
     width: 100%;
@@ -273,15 +283,6 @@ export default {
     left: 0%;
     display: flex;
     transition: all 0.5s;
-    .filesArea {
-    position: absolute;
-    left: -20%;
-    top: 0;
-    width: 20%;
-    height: 100%;
-    background-color: #fff;
-    transition: all 0.5s;
-  }
     .richText-left {
       height: 100%;
       width: 40%;
